d917cf3a8f8f6bc7353e23edc4279ae56d222be5
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-02-23 22:47:45 -0300

    Mantendo tela aberta

--- Tela.py
+++ Tela.py
@@ -9,29 +9,32 @@ class TelaPython():
             [sg.Text('Quais provedores de e-mail são aceitos?')],
             [sg.Checkbox('Gmail',key='Gmail'),sg.Checkbox('Outlook',key='Outlook'),sg.Checkbox("Yahoo",key='Yahoo')],
             [sg.Text('Aceita cartão?')],
-            [sg.Radio('sim','cartões',key='aceitaCartao'),sg.Radio('Não','cartões',key='naoAceitaCartao')],
+            [sg.Radio('sim','cartões',key='Aceita Cartão'),sg.Radio('Não','cartões',key='Não Aceita Cartão')],
             [sg.Button('Enviar')]
         ]
         #janela
-        janela = sg.Window("Dados do usuário").layout(layout)
-        #Extrair os dados da tela
-        self.button, self.values = janela.Read()
+        self.janela = sg.Window("Dados do usuário").layout(layout)
+
+
     
     def Iniciar(self):
-        nome = self.values['Nome']
-        idade = self.values['Idade']
-        g_mail = self.values['Gmail']
-        ou_tlook = self.values['Outlook']
-        ya_hoo = self.values['Yahoo']
-        aceita_cartão_sim = self.values['aceitaCartao']
-        nao_aceita_cartão = self.values['naoAceitaCartao']
-        print(f'Nome:{nome}')
-        print(f'Idade:{idade}')
-        print(f'Gmail:{g_mail}')
-        print(f'Outlook:{ou_tlook}')
-        print(f'Yahoo:{ya_hoo}')
-        print(f'aceitaCartao:{aceita_cartão_sim}')
-        print(f'naoAceitaCartao:{nao_aceita_cartão}')
+        while True: 
+          #Extrair os dados da tela
+          self.button, self.values = self.janela.Read()
+          nome = self.values['Nome']
+          idade = self.values['Idade']
+          g_mail = self.values['Gmail']
+          ou_tlook = self.values['Outlook']
+          ya_hoo = self.values['Yahoo']
+          aceita_cartão_sim = self.values['Aceita Cartão']
+          nao_aceita_cartão = self.values['Não Aceita Cartão']
+          print(f'Nome:{nome}')
+          print(f'Idade:{idade}')
+          print(f'Gmail:{g_mail}')
+          print(f'Outlook:{ou_tlook}')
+          print(f'Yahoo:{ya_hoo}')
+          print(f'Aceita Cartão:{aceita_cartão_sim}')
+          print(f'Não Aceita Cartâo:{nao_aceita_cartão}')
 
 
 tela = TelaPython()
